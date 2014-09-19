7ccdbcfd31ea631c32d61071029a77c42092c221
blobaugh <ben@lobaugh.net>
2014-09-19 14:47:27 -0700

    Converting rss feed content into htmlentities so that it can be stripped

--- plugins/rss-post-aggregate/includes/category_featured_images_widget.php
+++ plugins/rss-post-aggregate/includes/category_featured_images_widget.php
@@ -134,8 +134,8 @@ class RSS_Post_Aggregation_Category_Featured_Images_Widget extends WP_Widget {
 				$content = str_replace( '»', '', $p['summary'] );
 				$content = str_replace( 'Read more', '', $content );
 				$content = str_replace( '[...]', '', $content );
-				$content = str_replace( '&hellip;', '', $content );
-				$content = substr( $content, 0, -5 );
+				$content = str_replace( '&hellip;', '', htmlentities( $content ) );
+				//$content = substr( $content, 0, -5 );
 				
 				echo wpautop( $content );
 
