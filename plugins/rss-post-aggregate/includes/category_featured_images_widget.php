ca7e02e57878d82487873b84a804e860032915af
Justin Sternberg <justin@dsgnwrks.pro>
2014-09-22 11:05:32 -0400

    fix &hellip;

--- plugins/rss-post-aggregate/includes/category_featured_images_widget.php
+++ plugins/rss-post-aggregate/includes/category_featured_images_widget.php
@@ -132,10 +132,12 @@ class RSS_Post_Aggregation_Category_Featured_Images_Widget extends WP_Widget {
 				}
 
 				$content = str_replace( '»', '', $p['summary'] );
+				$content = str_replace( '[&hellip;]', '&hellip;', $content );
 				$content = str_replace( 'Read more', '', $content );
-				$content = str_replace( '&hellip;', '', htmlentities( $content, ENT_QUOTES, 'UTF-8' ) );
 				$content = str_replace( '[]', '', $content );
-				
+				$content = htmlentities( $content, ENT_QUOTES, 'UTF-8' );
+				$content = str_replace( '&hellip;', '…', $content );
+
 				echo wpautop( $content );
 
 				if ( isset( $instance['read_more_text'] ) && trim( $instance['read_more_text'] ) ) {
