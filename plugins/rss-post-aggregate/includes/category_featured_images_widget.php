e74c18589a684d66693f65e018fc134a40c22cf0
Jay Wood <jerry@webdevstudios.com>
2014-09-30 21:20:01 -0400

    Add widget visibility to feed image widget (quick fix)

--- plugins/rss-post-aggregate/includes/category_featured_images_widget.php
+++ plugins/rss-post-aggregate/includes/category_featured_images_widget.php
@@ -97,6 +97,12 @@ class RSS_Post_Aggregation_Category_Featured_Images_Widget extends WP_Widget {
 
 	public function widget( $args, $instance ) {
 
+		$should_show = apply_filters( 'rss_post_aggregation_images_widget_visibility', 'off' );
+		if( 'on' != $should_show ) {
+			echo '';
+			return;
+		}
+
 		echo $args['before_widget'];
 
 		echo $args['before_title'];
